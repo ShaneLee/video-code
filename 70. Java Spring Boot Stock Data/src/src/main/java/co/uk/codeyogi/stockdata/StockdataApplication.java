a8cf09df5a2fcfad0b82b11e6c67b5d5244db263
Shane Lee <me@shanel.ee>
2020-12-27 16:55:46 +0000

    Add 70

---
+++ 70. Java Spring Boot Stock Data/src/src/main/java/co/uk/codeyogi/stockdata/StockdataApplication.java
@@ -0,0 +1,13 @@
+package co.uk.codeyogi.stockdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockdataApplication.class, args);
+	}
+
+}
---
+++ 70. Java Spring Boot Stock Data/src/src/test/java/co/uk/codeyogi/stockdata/StockdataApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uk.codeyogi.stockdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
